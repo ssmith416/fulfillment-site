05b5054c037741130626d70aabc87e7006ce52d1
Sid Smith <88159306+ssmith416@users.noreply.github.com>
2023-07-14 13:37:13 -0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop < 50 || document.documentElement.scrollTop < 50) {
-        document.getElementById("header").style.fontSize = "40px";
-    } else {
-        document.getElementById("header").style.fontSize = "40px"
-    }
-}
