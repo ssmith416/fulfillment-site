f214290d1f362c0e58da748c4728f0e7eb68793c
Sid Smith <88159306+ssmith416@users.noreply.github.com>
2023-07-27 15:58:35 -0500

    Add files via upload

--- DFW Site/index.js
+++ DFW Site/index.js
@@ -1,9 +1,31 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop < 50 || document.documentElement.scrollTop < 50) {
-        document.getElementById("header").style.fontSize = "40px";
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("header").style.fontSize = "30px";
     } else {
-        document.getElementById("header").style.fontSize = "40px"
+        document.getElementById("header").style.fontSize = "50px";
     }
 }
+
+
+const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.7
+}
+
+function observerCallBack(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.replace('fadeOut', 'fadeIn');
+        } else {
+            entry.target.classList.replace('fadeIn', 'fadeOut');
+        }
+    });
+}
+
+const observer = new IntersectionObserver(observerCallBack, observerOptions);
+
+const fadeElms = document.querySelectorAll(".cards");
+fadeElms.forEach(el => observer.observe(el));
